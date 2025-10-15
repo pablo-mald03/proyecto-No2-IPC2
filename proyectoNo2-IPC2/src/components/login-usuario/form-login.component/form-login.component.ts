1f0aa2861ccafe99e41175d5f2be3ce8a5f92b31
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2025-10-15 01:30:18 -0600

    Se ha implementado el formulario de login reactivo

---
+++ proyectoNo2-IPC2/src/components/login-usuario/form-login.component/form-login.component.ts
@@ -0,0 +1,58 @@
+import { KeyValuePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLinkActive, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-form-login',
+  imports: [RouterLinkActive, RouterModule, FormsModule, ReactiveFormsModule, KeyValuePipe],
+  templateUrl: './form-login.component.html',
+  styleUrl: './form-login.component.scss'
+})
+export class FormLoginComponent implements OnInit {
+
+  loginFormulario!: FormGroup;
+  usuarioCredenciales!: Event;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.loginFormulario = this.formBuilder.group(
+      {
+        correo: [null, [Validators.required, Validators.maxLength(150), Validators.email]],
+        password: [null, [Validators.required, Validators.maxLength(150), Validators.minLength(5)]],
+      }
+    )
+
+  }
+
+  submit(): void {
+    if (this.loginFormulario.valid) {
+      this.usuarioCredenciales = this.loginFormulario.value as Event;
+
+      /*this.eventsService.createNewEvent(this.newEvent).subscribe({
+          next: () => {
+              this.reset();
+          },
+          error: (error: any) => {
+              console.log(error);
+          }
+      });*/
+
+      console.log(this.usuarioCredenciales);
+    }
+  }
+
+  reset(): void {
+    this.loginFormulario.reset({
+
+    });
+  }
+
+
+}
--- proyectoNo2-IPC2/src/pages/login-page/login-page.component.ts
+++ proyectoNo2-IPC2/src/pages/login-page/login-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterModule } from '@angular/router';
+import { FormLoginComponent } from "../../components/login-usuario/form-login.component/form-login.component";
 
 @Component({
   selector: 'app-login-page.component',
-  imports: [RouterModule, RouterLinkActive],
+  imports: [RouterModule, RouterLinkActive, FormLoginComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
