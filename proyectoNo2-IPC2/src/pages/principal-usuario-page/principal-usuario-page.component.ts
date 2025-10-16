241d07995aaed48d9851f8e82001acb4d3e3fe73
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2025-10-16 02:13:24 -0600

    Se han definido los componentes para el menu principal Inicio de rama

---
+++ proyectoNo2-IPC2/src/pages/principal-usuario-page/principal-usuario-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-usuario-page.component',
+  imports: [],
+  templateUrl: './principal-usuario-page.component.html',
+  styleUrl: './principal-usuario-page.component.scss'
+})
+export class PrincipalUsuarioPageComponent {
+
+}
