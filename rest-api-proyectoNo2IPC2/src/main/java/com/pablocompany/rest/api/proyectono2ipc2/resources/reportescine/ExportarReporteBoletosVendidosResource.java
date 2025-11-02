8977a6fd9662f03463c1a3a75ee3161f096e1ac0
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2025-11-02 05:08:21 -0600

    Se ha establecido los endpoints para poder comunicarse de frontEnd

---
+++ rest-api-proyectoNo2IPC2/src/main/java/com/pablocompany/rest/api/proyectono2ipc2/resources/reportescine/ExportarReporteBoletosVendidosResource.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pablocompany.rest.api.proyectono2ipc2.resources.reportescine;
+
+import com.pablocompany.rest.api.proyectono2ipc2.excepciones.ErrorInesperadoException;
+import com.pablocompany.rest.api.proyectono2ipc2.excepciones.FormatoInvalidoException;
+import com.pablocompany.rest.api.proyectono2ipc2.reportesadmincine.models.ReporteBoletosVendidosDTO;
+import com.pablocompany.rest.api.proyectono2ipc2.reportesadmincine.models.ReporteSalasGustadasDTO;
+import com.pablocompany.rest.api.proyectono2ipc2.reportesadmincine.services.ExportarBoletosVendidosService;
+import com.pablocompany.rest.api.proyectono2ipc2.reportesadmincine.services.ExportarSalasGustadasService;
+import com.pablocompany.rest.api.proyectono2ipc2.reportesadmincine.services.NombreReporteRandomService;
+import com.pablocompany.rest.api.proyectono2ipc2.reportesadmincine.services.ReporteSalasGustadasService;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.StreamingOutput;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author pablo
+ */
+@Path("reportes/boletos/vendidos/exportar")
+public class ExportarReporteBoletosVendidosResource {
+    
+    //Endpoint que permite exportar el reporte de boletos vendidos sin filtro
+    @GET
+    @Path("/inicio/{fechaInicio}/fin/{fechaFin}/limit/{limite}/offset/{inicio}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response reporteBoletosVendidosSinFiltro(
+            @PathParam("fechaInicio") String fechaInicio,
+            @PathParam("fechaFin") String fechaFin,
+            @PathParam("limite") String limite,
+            @PathParam("inicio") String inicio) {
+
+        ExportarBoletosVendidosService boletosVendidosService = new ExportarBoletosVendidosService();
+
+        try {
+            //PENDIENTE
+          /*  List<ReporteBoletosVendidosDTO> reporteSalasGustadasDto = boletosVendidosService.(fechaInicio, fechaFin, limite, inicio);
+
+             ExportarBoletosVendidosService boletosVendidosService = new ExportarBoletosVendidosService();
+*/
+            byte[] pdfGenerado = exportarSalasGustadasService.getReporteSalaGustada(reporteSalasGustadasDto);
+
+            StreamingOutput stream = output -> {
+
+                output.write(pdfGenerado);
+                output.flush();
+            };
+
+            NombreReporteRandomService nombreReporteRandomService = new NombreReporteRandomService();
+
+            String nombreRandom = nombreReporteRandomService.getNombre("ReporteSalasGustadas");
+
+            return Response.ok(stream, MediaType.APPLICATION_OCTET_STREAM)
+                    .header("Content-Disposition", "attachment; filename=\"" + nombreRandom + "\"")
+                    .header("Access-Control-Expose-Headers", "Content-Disposition")
+                    .build();
+
+        } catch (FormatoInvalidoException ex) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(Map.of("mensaje", ex.getMessage())).build();
+        } catch (ErrorInesperadoException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Map.of("mensaje", ex.getMessage())).build();
+        }
+
+    }
+
+    //Endpoint que permite exportar el reporte de los boletos vendidos con filtro
+    @GET
+    @Path("/inicio/{fechaInicio}/fin/{fechaFin}/filtro/{idSala}/limit/{limite}/offset/{inicio}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response reporteBoletosVendidosFiltro(
+            @PathParam("fechaInicio") String fechaInicio,
+            @PathParam("fechaFin") String fechaFin,
+            @PathParam("limite") String limite,
+            @PathParam("inicio") String inicio,
+            @PathParam("idSala") String idSala) {
+
+        ReporteSalasGustadasService reporteSalasGustadasService = new ReporteSalasGustadasService();
+
+        try {
+            List<ReporteSalasGustadasDTO> reporteSalasGustadasDto = reporteSalasGustadasService.obtenerreporteSalaGustadaFiltro(fechaInicio, fechaFin, limite, inicio, idSala);
+
+            ExportarSalasGustadasService exportarSalasGustadasService = new ExportarSalasGustadasService();
+
+            byte[] pdfGenerado = exportarSalasGustadasService.getReporteSalaGustada(reporteSalasGustadasDto);
+
+            StreamingOutput stream = output -> {
+
+                output.write(pdfGenerado);
+                output.flush();
+            };
+
+            NombreReporteRandomService nombreReporteRandomService = new NombreReporteRandomService();
+
+            String nombreRandom = nombreReporteRandomService.getNombre("ReporteSalasGustadas");
+
+            return Response.ok(stream, MediaType.APPLICATION_OCTET_STREAM)
+                    .header("Content-Disposition", "attachment; filename=\"" + nombreRandom + "\"")
+                    .header("Access-Control-Expose-Headers", "Content-Disposition")
+                    .build();
+
+        } catch (FormatoInvalidoException ex) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(Map.of("mensaje", ex.getMessage())).build();
+        } catch (ErrorInesperadoException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Map.of("mensaje", ex.getMessage())).build();
+        }
+
+    }
+    
+}
